2b11b2261399913611e9afcadf89ddbfd65b6404
Mitchell Budge <mitchellbudge@gmail.com>
2019-05-01 15:57:32 -0600

    Created a new Xcode file.

---
+++ PalindromeChecker2.0/PalindromeChecker2.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PalindromeChecker2.0
+//
+//  Created by Mitchell Budge on 5/1/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
